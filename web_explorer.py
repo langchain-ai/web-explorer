48e32dbd74122690f03ce6af1505693274f1d93c
Lance Martin <lance@langchain.dev>
2023-07-26 12:54:52 -0700

    Add streaming

--- web_explorer.py
+++ web_explorer.py
@@ -1,10 +1,11 @@
 import os
 import streamlit as st
+from langchain.callbacks.base import BaseCallbackHandler
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.retrievers.web_research import WebResearchRetriever
 
 @st.cache_resource
-def settings():
+def settings(_stream_handler):
 
     # Vectorstore
     import faiss
@@ -18,7 +19,7 @@ def settings():
 
     # LLM
     from langchain.chat_models import ChatOpenAI
-    llm = ChatOpenAI(temperature=0)
+    llm = ChatOpenAI(temperature=0, streaming=True, callbacks=[stream_handler])
 
     # Search
     from langchain.utilities import GoogleSearchAPIWrapper
@@ -34,6 +35,32 @@ def settings():
 
     return web_retriever, llm
 
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text=""):
+        self.container = container
+        self.text = initial_text
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token
+        self.container.markdown(self.text)
+
+stream_handler = StreamHandler(st.empty())
+
+class PrintRetrievalHandler(BaseCallbackHandler):
+    def __init__(self, container):
+        self.container = container.expander("Context Retrieval")
+
+    def on_retriever_start(self, query: str, **kwargs):
+        self.container.write(f"**Question:** {query}")
+
+    def on_retriever_end(self, documents, **kwargs):
+        # self.container.write(documents)
+        for idx, doc in enumerate(documents):
+            source = os.path.basename(doc.metadata["source"])
+            self.container.write(f"**Document {idx} from {source}**")
+            self.container.markdown(doc.page_content)
+
+
 st.sidebar.image("img/ai.png")
 st.header("`Interweb Explorer`")
 st.info("`I am an AI that can answer questions by exploring, reading, and summarizing web pages."
@@ -46,7 +73,7 @@ os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_API_KEY"]
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
 # Make retriever and llm
-web_retriever, llm = settings()
+web_retriever, llm = settings(stream_handler)
 
 # User input 
 question = st.text_input("`Ask a question:`")
@@ -61,7 +88,8 @@ if question:
                                                            retriever=web_retriever)
     
     # Write answer and sources
-    result = qa_chain({"question": question})
+    retrieval_streamer_cb = PrintRetrievalHandler(st.container())
+    result = qa_chain({"question": question},callbacks=[retrieval_streamer_cb, stream_handler])
     st.info('`Answer:`')
     st.info(result['answer'])
     st.info('`Source:`')
